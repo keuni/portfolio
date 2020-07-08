0565cc3c1e1e20760f4d34fb58c09bd8cb6b602f
keuni <kyungeun.kiim@gmail.com>
2020-07-08 20:48:06 +0900

    github deploy test

--- client/src/components/portfolio/PortfolioModal.js
+++ client/src/components/portfolio/PortfolioModal.js
@@ -65,7 +65,7 @@ class PortfolioModal extends React.Component {
   }
 
   getAllData() {
-    fetch('http://localhost:4000/data', {
+    fetch('http://15.164.102.207:4000/data', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -163,7 +163,7 @@ class PortfolioModal extends React.Component {
 
   stepOne() {
     if (this.state.job) {
-      fetch('http://localhost:4000/job', {
+      fetch('http://15.164.102.207:4000/job', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -275,7 +275,7 @@ class PortfolioModal extends React.Component {
     let { id, ability } = this.state;
 
     if (ability && ability.length === 5) {
-      fetch('http://localhost:4000/ability', {
+      fetch('http://15.164.102.207:4000/ability', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -388,7 +388,7 @@ class PortfolioModal extends React.Component {
   stepFive() {
     let { id, company, developer } = this.state;
 
-    fetch('http://localhost:4000/company', {
+    fetch('http://15.164.102.207:4000/company', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -482,7 +482,7 @@ class PortfolioModal extends React.Component {
     let { id, interest } = this.state;
     interest = interest === 'no' ? false : true;
     if (interest !== null) {
-      fetch('http://localhost:4000/interest', {
+      fetch('http://15.164.102.207:4000/interest', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
