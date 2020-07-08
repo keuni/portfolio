09b74a9c3af82bb08271d362e0823b71f1c95c48
keuni <kyungeun.kiim@gmail.com>
2020-07-08 20:47:37 +0900

    Updates

---
+++ precache-manifest.e3a3ecbf3f765ce385c5f860632af8a1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2678ca5ccff31f9eb5989de2aea45071",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2bb0a0d8df19586eda96",
+    "url": "/portfolio/static/css/main.b92d5bbe.chunk.css"
+  },
+  {
+    "revision": "3bcd35f64254a26e01c4",
+    "url": "/portfolio/static/js/2.10694507.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/portfolio/static/js/2.10694507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bb0a0d8df19586eda96",
+    "url": "/portfolio/static/js/main.fcfd57c8.chunk.js"
+  },
+  {
+    "revision": "4dea65de810090fb90d4",
+    "url": "/portfolio/static/js/runtime-main.f869d034.js"
+  },
+  {
+    "revision": "c77de5de1b2f79768fc8f714a9833de5",
+    "url": "/portfolio/static/media/beHappy_main.c77de5de.png"
+  },
+  {
+    "revision": "8cba083f22a3cb2eddf74d934a084256",
+    "url": "/portfolio/static/media/email.8cba083f.png"
+  },
+  {
+    "revision": "c9a2ee417db1a618598169c154ed6725",
+    "url": "/portfolio/static/media/zeroTo66_main.c9a2ee41.png"
+  }
+]);
