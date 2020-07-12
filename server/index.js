8bccf25e8caad79785c2a3002aaf9078a1db51f4
keuni <kyungeun.kiim@gmail.com>
2020-07-12 14:55:02 +0900

    FIX for cors Error

--- server/index.js
+++ server/index.js
@@ -14,12 +14,13 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: [
-      'http://keuni.github.io',
-      'https://keuni.github.io',
-      'http://localhost:3000',
-      'http://keuni-portfolio.s3-website.ap-northeast-2.amazonaws.com/',
-    ],
+    origin: 'http://keuni-portfolio.s3-website.ap-northeast-2.amazonaws.com/',
+    // [
+    //   'http://keuni.github.io',
+    //   'https://keuni.github.io',
+    //   'http://localhost:3000',
+    //   'http://keuni-portfolio.s3-website.ap-northeast-2.amazonaws.com/',
+    // ]
     method: ['GET', 'POST', 'OPTIONS'],
     credentials: true,
   })
